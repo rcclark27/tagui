8faa6f4c28781d843a9c6e80a7da3926021efe37
rcclark27 <91893143+rcclark27@users.noreply.github.com>
2021-10-14 08:41:37 -0500

    Home page and create account page added

--- src/main/java/com/tagui/Controller.java
+++ src/main/java/com/tagui/Controller.java
@@ -1,25 +1,56 @@
 package com.tagui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+//import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.text.Text;
+//import javafx.scene.text.Text;
 import javafx.scene.control.TextField;
+//import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.scene.Node;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class Controller
 {
-    @FXML
-    Label tfMessage;
+    @FXML Label tfMessage;
+    @FXML Label tfOutput;
+    @FXML TextField userLogin;
 
-    @FXML
-    Label tfOutput;
-
-    @FXML
-    TextField userLogin;
-
-    public void click()
+    /*public void click()
     {
         String message = userLogin.getText();
         String output = "Hello " + message;
         tfOutput.setText(output);
+    }*/
+
+    public void showHomePage(ActionEvent event) throws IOException
+    {
+        Parent homeParent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("homePage.fxml")));
+        Scene homeScene = new Scene(homeParent);
+
+        //This gets the Stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(homeScene);
+        window.show();
     }
+
+    public void createAccount(ActionEvent event) throws IOException
+    {
+        Parent createAccountParent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("createaccount.fxml")));
+        Scene createAccountScene = new Scene(createAccountParent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(createAccountScene);
+        window.show();
+
+    }
+
 }
